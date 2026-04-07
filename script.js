bf96e22bc9dce033a89e98b3791f70b36920c694
Rajdeepniranjan0041 <rajdeep2nd789@gmail.com>
2026-04-08 03:10:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,109 +1,37 @@
-var employees = [];
+let heading = document.getElementById("mainHeading");
+let paragraph = document.getElementById("paragraph");
+let input = document.getElementById("input");
 
-function addEmployee(){
+let fontSize = 16;
 
-var name = document.getElementById("name").value;
-var id = document.getElementById("id").value;
-var salary = document.getElementById("salary").value;
-var dept = document.getElementById("dept").value;
+document.getElementById("changeTextBtn").addEventListener("click", function() {
+    if (input.value !== "") {
+        heading.innerHTML = input.value;
+    }
+});
 
-var emp = {
-name: name,
-id: id,
-salary: salary,
-dept: dept
+document.getElementById("bgColorBtn").onclick = function() {
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 };
 
-employees.push(emp);
+document.getElementById("fontSizeBtn").addEventListener("click", function() {
+    fontSize += 2;
+    paragraph.style.fontSize = fontSize + "px";
+});
 
-document.getElementById("output").innerHTML = "Employee Added";
-}
+document.getElementById("toggleBtn").addEventListener("click", function() {
+    if (paragraph.style.display === "none") {
+        paragraph.style.display = "block";
+    } else {
+        paragraph.style.display = "none";
+    }
+});
 
-
-function displayEmployees(){
-
-var text = "<b>All Employees</b><br>";
-
-for(var i=0;i<employees.length;i++){
-
-text += "Name: " + employees[i].name +
-        " | ID: " + employees[i].id +
-        " | Salary: ₹" + employees[i].salary +
-        " | Dept: " + employees[i].dept + "<br>";
-
-}
-
-document.getElementById("output").innerHTML = text;
-}
-
-
-function salaryFilter(){
-
-var text = "<b>Employees with Salary > ₹50,000</b><br>";
-
-for(var i=0;i<employees.length;i++){
-
-if(employees[i].salary > 50000){
-
-text += "Name: " + employees[i].name +
-        " Salary: ₹" + employees[i].salary + "<br>";
-
-}
-
-}
-
-document.getElementById("output").innerHTML = text;
-}
-
-
-function totalSalary(){
-
-var total = 0;
-
-for(var i=0;i<employees.length;i++){
-
-total = total + Number(employees[i].salary);
-
-}
-
-document.getElementById("output").innerHTML =
-"<b>Total Salary Payout:</b> ₹" + total;
-}
-
-
-function averageSalary(){
-
-var total = 0;
-
-for(var i=0;i<employees.length;i++){
-
-total = total + Number(employees[i].salary);
-
-}
-
-var avg = total / employees.length;
-
-document.getElementById("output").innerHTML =
-"<b>Average Salary:</b> ₹" + avg;
-}
-
-
-function countDepartment(){
-
-var deptName = prompt("Enter Department Name");
-
-var count = 0;
-
-for(var i=0;i<employees.length;i++){
-
-if(employees[i].dept == deptName){
-
-count++;
-
-}
-
-}
-
-document.getElementById("output").innerHTML =
-"Employees in " + deptName + ": " + count;
-}
+document.getElementById("resetBtn").addEventListener("click", function() {
+    heading.innerHTML = "Welcome to JavaScript Lab";
+    paragraph.style.display = "block";
+    document.body.style.backgroundColor = "white";
+    fontSize = 16;
+    paragraph.style.fontSize = fontSize + "px";
+    input.value = "";
+});
